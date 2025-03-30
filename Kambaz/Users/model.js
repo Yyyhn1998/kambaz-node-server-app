d85fd707d31f4469fe7ef23f8da36744f22a5af2
yyyhn1998 <yin.haon@northeastern.edu>
2025-03-30 15:32:43 -0400

    finally mooge edit

--- Kambaz/Users/model.js
+++ Kambaz/Users/model.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import schema from "./schema.js";
-const model = mongoose.model("UserModel", schema);
+
+const db = mongoose.connection.useDb("kambaz");
+const model = db.model("UserModel", schema, "users");
+
 export default model;
 
