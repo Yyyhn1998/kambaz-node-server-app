60abfbad49377daac5d533ef83a09f814c2d0a5f
yyyhn1998 <yin.haon@northeastern.edu>
2025-03-30 13:54:05 -0400

    cookie fix again

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Hello from "./hello.js"
+import Hello from "./hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
@@ -12,43 +12,49 @@ import enrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 import PeopleRoutes from "./Kambaz/People/routes.js";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+const CONNECTION_STRING =
+    process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
 mongoose.connect(CONNECTION_STRING);
-const app = express()
+
+const app = express();
+
 app.use(
     cors({
              credentials: true,
              origin: [
-                 process.env.NETLIFY_URL || "https://nenemomo-neu-1998.netlify.app",
+                 "https://nenemomo-neu-1998.netlify.app",
                  "http://localhost:5173"
              ]
          })
 );
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kambaz",
     resave: false,
     saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
         sameSite: "none",
         secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
+        domain: process.env.NODE_SERVER_DOMAIN || "kambaz-node-server-app-1-qg4v.onrender.com"
     };
 }
 
-app.use(
-    session(sessionOptions)
-);
+app.use(session(sessionOptions));
 
 app.use(express.json());
+
+
 UserRoutes(app);
-Hello(app)
+Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 enrollmentsRoutes(app);
 PeopleRoutes(app);
 Lab5(app);
-app.listen(process.env.PORT || 4000)
+
+app.listen(process.env.PORT || 4000);
