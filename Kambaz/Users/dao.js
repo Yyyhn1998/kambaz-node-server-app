858a9b70911678b413e6364b0abf1a761b609ba3
yyyhn1998 <yin.haon@northeastern.edu>
2025-03-30 13:10:46 -0400

    add mongo connection + deploy config

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,13 +1,15 @@
+import model from "./model.js";
 import db from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 import Database from "../Database/index.js";
 let { users } = db;
-export const createUser = (user) => {
-    const newUser = { ...user, _id: uuidv4() };
-    users = [...users, newUser];
-    return newUser;
-};
-export const findUserByUsername = (username) => users.find((user) => user.username === username);
+
+export const findAllUsers = () => model.find();
+export const findUserById = (userId) => model.findById(userId);
+export const findUserByUsername = (username) =>  model.findOne({ username: username });
+export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+/*export const findUserByUsername = (username) => users.find((user) => user.username === username);
 export const findAllUsers = () => users;
 export const findUserById = (userId) => users.find((user) => user._id === userId);
 export const findUserByCredentials = (username, password) =>
@@ -18,9 +20,25 @@ export const deleteUser = (userId) => (users = users.filter((u) => u._id !== use
 export function findAllCourses() {
     return Database.courses;
 }
+*/
 export function findCoursesForEnrolledUser(userId) {
     const { courses, enrollments } = Database;
     const enrolledCourses = courses.filter((course) =>
                                                enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
     return enrolledCourses;
+}
+
+export const findUsersByRole = (role) => model.find({ role: role });
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+                          $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+                      });
+};
+
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+
+export const createUser = (user) => {
+    const newUser = { ...user, _id: uuidv4() };
+    return model.create(newUser);
 }
